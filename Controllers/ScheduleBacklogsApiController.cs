849a7f02299f314d28dae455349414dca2257315
Lugubriszxc <@Lugubriszxc>
2024-04-23 00:51:45 +0800

    back to reset, transfered some of codes

--- Controllers/ScheduleBacklogsApiController.cs
+++ Controllers/ScheduleBacklogsApiController.cs
@@ -29,6 +29,13 @@ namespace matikApp.Controllers
             public string SectionName { get; set; }
         }
 
+        public class SubjectSection
+        {
+            public int SectionID {get; set;}
+            public string SectionName { get; set; }
+            public string SubjectName { get; set; }
+        }
+
         public IActionResult instructorBacklogs(int acadVal, string semesterVal)
         {
 
@@ -88,6 +95,45 @@ namespace matikApp.Controllers
                 }
             }
 
+            return Ok(backlogDetected);
+        }
+
+        public IActionResult subjectBacklogs(int acadVal, string semesterVal)
+        {
+            List<SubjectSection> backlogDetected = new List<SubjectSection> { };
+
+            var checkSection = _context.Regissections.Where(s => s.AcadYearId == acadVal && s.Semester == semesterVal).ToList();
+
+            foreach (var sec in checkSection)
+            {
+                //get the section detail from regissections 
+                var getSec = _context.Sections.Where(s => s.SectionId == sec.SectionId).FirstOrDefault();
+                if (getSec != null)
+                {
+                    //getting the subjects from that section
+                    var getAssignSub = _context.Assignsubjects.Where(asub => asub.CourseId == getSec.CourseId && asub.YearLevel == getSec.YearLevel && asub.Semester == semesterVal).ToList();
+                    foreach (var subJ in getAssignSub)
+                    {
+                        bool subjectExists = _context.Roomschedules.Any(rs => rs.SectionId == sec.SectionId && rs.SubjectId == subJ.SubjectId && rs.AcadYearId == acadVal && rs.Semester == semesterVal);
+                        if (!subjectExists)
+                        {
+                            var getSub = _context.Subjects.Where(s => s.SubjectId == subJ.SubjectId).FirstOrDefault();
+                            if (getSub != null)
+                            {
+                                Console.WriteLine("This section : " + getSec.SectionName + "Don't have : " + getSub.SubjectName);
+                                SubjectSection subGet = new SubjectSection
+                                {
+                                    SectionID = getSec.SectionId,
+                                    SectionName = getSec.SectionName,
+                                    SubjectName = getSub.SubjectName
+                                };
+
+                                backlogDetected.Add(subGet);
+                            }
+                        }
+                    }
+                }
+            }
             return Ok(backlogDetected);
         }
     }
